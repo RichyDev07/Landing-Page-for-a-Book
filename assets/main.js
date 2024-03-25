bfd874aa101b342a13ee1f1512c423be18b93ad7
RichyDev07 <richmondgyening007@gmail.com>
2024-03-25 19:15:38 +0000

    Finalized navigation bar and landing page design

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+function taggleMenu(displaystate) {
+    const nav = document.getElementById('header-navigation');
+    const closeIco = document.getElementById('close-ico');
+    nav.style.display = displaystate;
+    closeIco.style.display = displaystate 
+    
+}
